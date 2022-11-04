e968080226373f65cd4964d5e4e2824a41a35d78
lucasdmmc <lucas.dmmc@hotmail.com>
2022-11-04 18:56:44 -0300

    Criei as rodas da aplicação

--- copa/server/src/server.ts
+++ copa/server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
+import zod  from "zod"
 import { PrismaClient } from "@prisma/client"
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -21,6 +23,37 @@ async function bootstrap() {
     return { count }
   })
 
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count()
+
+    return { count }
+  })
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count()
+
+    return { count }
+  })
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = zod.object({
+      title: zod.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+    
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    })
+    return reply.status(201).send({ code })
+  })
+  
   await fastify.listen({ port: 3333, /*host: "0.0.0.0"*/ })
 }
 
