a22e9dc9ad90ed247bd9879d6b7d638713bf645f
lucasdmmc <lucas.dmmc@hotmail.com>
2022-11-08 20:58:50 -0300

    Adicionei novas rotas na aplicação

--- copa/server/src/server.ts
+++ copa/server/src/server.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   fastify.register(guessRoutes)
   fastify.register(userRoutes)
 
-  await fastify.listen({ port: 3333, /*host: "0.0.0.0"*/ })
+  await fastify.listen({ port: 3333, host: "0.0.0.0" })
 }
 
 bootstrap()
